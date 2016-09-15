f8046b477fb0093c6228d817ade16c9239721691
Kamilla <holanda.kamilla@gmail.com>
2016-09-15 18:40:30 -0300

    Adding landing page

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -8,6 +8,7 @@ module.exports = function(defaults) {
   });
 
   app.import("vendor/adminlte/app.js");
+  app.import("vendor/landing/smoothscroll.js");
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
