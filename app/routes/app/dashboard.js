da58d6b5176cc7dd59baccc07afeb3de0cd1fdb0
Kamilla <holanda.kamilla@gmail.com>
2016-09-18 14:31:21 -0300

    Fixin body classes

--- app/routes/app/dashboard.js
+++ app/routes/app/dashboard.js
@@ -2,9 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     activate: function() {
-        Ember.$('body').toggleClass("sidebar-mini");
+        Ember.$('body').toggleClass("hold-transition skin-blue sidebar-mini");
     },
     deactivate: function(){
-        Ember.$('body').toggleClass("sidebar-mini");
+        Ember.$('body').toggleClass("hold-transition skin-blue sidebar-mini");
     }
 });
--- app/routes/login.js
+++ app/routes/login.js
@@ -2,9 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     activate: function() {
-        Ember.$('body').toggleClass("login-page");
+        Ember.$('body').toggleClass("hold-transition login-page");
     },
     deactivate: function(){
-        Ember.$('body').toggleClass("login-page");
+        Ember.$('body').toggleClass("hold-transition login-page");
     }
 });
